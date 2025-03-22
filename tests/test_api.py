7b937680833011b344f150b1fff4eac4c2ed0836
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-03-23 00:00:58 +0500

    add API tests

---
+++ tests/test_api.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+
+def test_predict_endpoint():
+    url = "http://localhost:3000/predict"
+    payload = {
+        "input_data": {
+            "MedInc": 8.3252,
+            "HouseAge": 41.0,
+            "AveRooms": 6.984127,
+            "AveBedrms": 1.023810,
+            "Population": 322.0,
+            "AveOccup": 2.555556,
+            "Latitude": 37.88,
+            "Longitude": -122.23,
+        },
+    }
+
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+
+
+def test_health_endpoint():
+    url = "http://localhost:3000/health"
+    response = requests.get(url)
+
+    assert response.status_code == 200
+    assert response.text == "OK"
+
+
+def test_metrics_endpoint():
+    url = "http://localhost:3000/metrics"
+    response = requests.get(url)
+
+    assert response.status_code == 200
