4338587351854df43318026d01622e320bbd5cd6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-03-22 22:14:31 +0500

    add Prometheus for monitoring prediction metrics

--- src/california_housing_api/service.py
+++ src/california_housing_api/service.py
@@ -3,6 +3,9 @@ import joblib
 import numpy as np
 from pydantic import BaseModel
 
+from prometheus_client import Summary, Counter, generate_latest
+import time
+
 
 class HousingFeatures(BaseModel):
     MedInc: float
@@ -17,15 +20,31 @@ class HousingFeatures(BaseModel):
 
 model = joblib.load("models/housing_model.joblib")
 
+# prometheus metrics
+PREDICTION_TIME = Summary("prediction_time", "Time spent on Predictions")
+PREDICTION_COUNTER = Counter("prediction_counter", "No. of Predictions")
+
 
 @bentoml.service(name="housing_predictor", runners=[])
 class HousingPredictor:
     @bentoml.api
     def predict(self, input_data: HousingFeatures) -> np.ndarray:
+        start = time.time()
+
         input_array = np.array(list(input_data.dict().values())).reshape(1, -1)
         prediction = model.predict(input_array)
+
+        end = time.time()
+
+        PREDICTION_TIME.observe(end - start)
+        PREDICTION_COUNTER.inc()
+
         return prediction
 
     @bentoml.api(route="/health")
     def health(self) -> str:
         return "OK"
+
+    @bentoml.api(route="/metrics")
+    def metrics(self) -> str:
+        return generate_latest().decode()
